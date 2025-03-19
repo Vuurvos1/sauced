08458d61154194ad41d759d9cd923a3eeac3998c
Sam <47493725+Vuurvos1@users.noreply.github.com>
2025-03-19 18:31:15 +0100

    setup account settings page

---
+++ apps/site/src/routes/profile/[slug]/settings/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<section>
+	<h1>Edit Profile</h1>
+</section>
+
+<section>
+	<h2>Basic Information</h2>
+
+	<form action="">
+		<!-- email -->
+
+		<!-- username -->
+
+		<!-- if changed, show save button -->
+	</form>
+
+	<!-- reset password -->
+</section>
+
+<section>
+	<h2>Danger Zone</h2>
+
+	<p>Actions in this section can permanently delete your data.</p>
+	<!-- danger zone -->
+
+	<div class="flex flex-col gap-2">
+		<h3 class="font-medium">Delete Account</h3>
+		<p class="text-muted-foreground mb-2 text-sm">
+			Permanently delete your account and all of your content. This action cannot be undone.
+		</p>
+		<div>
+			<!-- <DeleteAccountDialog /> -->
+		</div>
+	</div>
+
+	<form action=""></form>
+</section>
