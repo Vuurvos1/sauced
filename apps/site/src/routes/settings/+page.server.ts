61f0c651dcdfecffcf22be9a1e77c956cefe1ee3
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2025-03-21 23:37:58 +0100

    upgrade user page

--- apps/site/src/routes/settings/+page.server.ts
+++ apps/site/src/routes/settings/+page.server.ts
@@ -1,7 +1,56 @@
-import { redirect } from '@sveltejs/kit';
+import { invalidateSession, deleteSessionTokenCookie } from '$lib/server/session';
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { db } from '$lib/db';
+import { userTable } from '@app/db/schema';
+import { eq } from 'drizzle-orm';
 
 export async function load({ locals: { user } }) {
 	if (!user) {
 		redirect(302, '/');
 	}
 }
+
+export const actions: Actions = {
+	updateUser: async ({ locals, request }) => {
+		if (!locals.session) {
+			return fail(401);
+		}
+
+		const formData = await request.formData();
+		const username = formData.get('username') as string | null;
+
+		if (username) {
+			try {
+				await db.update(userTable).set({ username }).where(eq(userTable.id, locals.session.userId));
+			} catch (error) {
+				console.error(error);
+				return fail(500, {
+					message: 'Failed to update username'
+				});
+			}
+		}
+	},
+	deleteAccount: async ({ locals, cookies, request }) => {
+		if (!locals.session) {
+			return fail(401);
+		}
+
+		const formData = await request.formData();
+		const confirm = formData.get('confirm');
+
+		if (confirm !== 'DELETE') {
+			return fail(400, {
+				message: 'Invalid confirmation'
+			});
+		}
+
+		// delete user
+		await db.delete(userTable).where(eq(userTable.id, locals.session.userId));
+
+		// invalidate session
+		await invalidateSession(locals.session.id);
+		deleteSessionTokenCookie(cookies);
+
+		return redirect(302, '/');
+	}
+};
--- apps/site/src/routes/settings/+page.svelte
+++ apps/site/src/routes/settings/+page.svelte
@@ -1,43 +1,71 @@
 <script lang="ts">
+	import TextInput from '$lib/components/form/TextInput.svelte';
+	import DeleteAccountDialog from './DeleteAcountDialog.svelte';
+	import { enhance } from '$app/forms';
+	import { redirect } from '@sveltejs/kit';
+
+	let { data } = $props();
+
+	let { user } = $derived(data);
+
+	// svelte-ignore state_referenced_locally
+	if (!user) {
+		redirect(302, '/');
+	}
 </script>
 
-<section>
+<section class="container mb-6">
 	<h1 class="h2">Edit Profile</h1>
 </section>
 
-<section>
-	<h2>Basic Information</h2>
+<div class="space-y-4">
+	<section>
+		<div class="container">
+			<div class="card p-6">
+				<h2 class="mb-6 text-xl font-semibold">Basic Information</h2>
 
-	<form action="">
-		<!-- email -->
+				<form method="post" action="?/updateUser" class="space-y-6" use:enhance>
+					<TextInput label="Username" name="username" value={user.username} />
 
-		<!-- username -->
+					<!-- if changed, show save button -->
+					<div class="flex justify-end">
+						<button type="submit" class="btn btn-primary">Save Changes</button>
+					</div>
+				</form>
 
-		<!-- if changed, show save button -->
-	</form>
+				<div class="mt-8 border-t border-gray-200 pt-6">
+					<div class="flex flex-col gap-4">
+						<a href="/auth/forgot-password" class="font-medium text-blue-600 hover:text-blue-800">
+							Reset Password
+						</a>
 
-	<!-- reset password -->
-</section>
-
-<form method="post" action="/?/logout">
-	<button type="submit">Logout</button>
-</form>
-
-<section>
-	<div class="card border-red-500 bg-red-500/10">
-		<h2>Danger Zone</h2>
-
-		<p>Actions in this section can permanently delete your data.</p>
-		<!-- danger zone -->
-
-		<div class="flex flex-col gap-2">
-			<h3 class="font-medium">Delete Account</h3>
-			<p class="text-muted-foreground mb-2 text-sm">
-				Permanently delete your account and all of your content. This action cannot be undone.
-			</p>
-			<div>
-				<!-- <DeleteAccountDialog /> -->
+						<form method="post" action="/?/logout">
+							<button type="submit" class="btn btn-outline">Logout</button>
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+
+	<section>
+		<div class="container">
+			<div class="card border-red-500 bg-red-400/10">
+				<h2 class="h3 mb-2 text-red-500">Danger Zone</h2>
+
+				<p class="mb-6 text-gray-500">Actions in this section can permanently delete your data.</p>
+				<!-- danger zone -->
+
+				<div class="flex flex-col gap-2">
+					<h3 class="font-medium">Delete Account</h3>
+					<p class="mb-2 text-sm text-gray-500">
+						Permanently delete your account and all of your content. This action cannot be undone.
+					</p>
+					<div>
+						<DeleteAccountDialog></DeleteAccountDialog>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
